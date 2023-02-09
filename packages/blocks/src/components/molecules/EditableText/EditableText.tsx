c176b6b13181753a1f0351a54645c65e90155ecd
german-mergestat <german@mergestat.com>
2023-02-09 17:46:29 -0500

    chore(saved-queries): Adjusting Editable Text component to handle readonly

--- packages/blocks/src/components/molecules/EditableText/EditableText.tsx
+++ packages/blocks/src/components/molecules/EditableText/EditableText.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 import { ColoredBox } from '../../atoms/ColoredBox';
 import { Input } from '../../atoms/Form';
 
+interface InputEditableText extends React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> {
+  error?: boolean
+}
+
 export type EditableTextProps = {
   className?: string
   icon?: React.ReactElement
-  title: React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  >
-  desc: React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  >
+  title: InputEditableText
+  desc: InputEditableText
 }
 
 export const EditableText: React.FC<EditableTextProps> = ({ className, icon, title, desc }) => {
@@ -25,8 +26,22 @@ export const EditableText: React.FC<EditableTextProps> = ({ className, icon, tit
         {icon}
       </ColoredBox>}
       <div className='ml-2 flex-grow'>
-        <Input variant="ghost" className='t-input-title leading-none' {...title} />
-        <Input variant="ghost" className='t-input-desc leading-none' {...desc} />
+        <Input variant={title.error ? 'ghost-error' : 'ghost'}
+          className={cx(
+            't-input-title leading-none', {
+            't-input-readonly': title.readOnly,
+            't-input-onclick': !!title.onClick
+          })}
+          {...title}
+        />
+        <Input variant={desc.error ? 'ghost-error' : 'ghost'}
+          className={cx(
+            't-input-desc leading-none', {
+            't-input-readonly': desc.readOnly,
+            't-input-onclick': !!desc.onClick
+          })}
+          {...desc}
+        />
       </div>
     </div>
   )

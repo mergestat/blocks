b78f0c1f73fb8085acf7e6baeb6f0cf8dbbc5d9f
german-mergestat <german@mergestat.com>
2023-03-08 16:27:16 -0500

    feat(radio-card): Adding disabled and tootip feature for RadioCard component

--- packages/blocks/src/components/atoms/RadioCard/RadioCard.stories.tsx
+++ packages/blocks/src/components/atoms/RadioCard/RadioCard.stories.tsx
@@ -12,14 +12,29 @@ const Template: ComponentStory<typeof RadioCard> = (args) => <RadioCard {...args
 
 export const Default = Template.bind({})
 Default.args = {
-  startIcon: <GithubIcon className="t-icon t-icon-default" />,
+  startIcon: <GithubIcon className="t-icon" />,
   label: "Option",
   isSelected: false
 }
 
 export const Selected = Template.bind({})
 Selected.args = {
-  startIcon: <GithubIcon className="t-icon t-icon-primary" />,
+  startIcon: <GithubIcon className="t-icon" />,
   label: "Option",
   isSelected: true
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  startIcon: <GithubIcon className="t-icon" />,
+  label: "Option",
+  disabled: true
+}
+
+export const Tooltip = Template.bind({})
+Tooltip.args = {
+  startIcon: <GithubIcon className="t-icon" />,
+  label: "Option",
+  disabled: true,
+  tooltip: 'This field is disabled'
 }
--- packages/blocks/src/components/atoms/RadioCard/RadioCard.tsx
+++ packages/blocks/src/components/atoms/RadioCard/RadioCard.tsx
@@ -1,11 +1,15 @@
+import { CircleInformationIcon } from '@mergestat/icons';
 import cx from 'classnames';
 import React from 'react';
+import { Tooltip } from '../Tooltip';
 
 type RadioCardProps = {
   className?: string | undefined;
   startIcon?: React.ReactNode;
   isSelected?: boolean;
+  disabled?: boolean;
   label?: string;
+  tooltip?: string;
   onChange?: () => void;
 };
 
@@ -13,7 +17,9 @@ export const RadioCard: React.FC<RadioCardProps> = ({
   className,
   startIcon,
   isSelected = false,
+  disabled = false,
   label,
+  tooltip,
   onChange,
 }) => {
   const _classname = className ? { [className]: !!className } : {}
@@ -22,14 +28,19 @@ export const RadioCard: React.FC<RadioCardProps> = ({
     <div
       className={cx('t-radio-card', {
         't-radio-card-selected': isSelected,
+        't-radio-card-disabled': disabled,
         ..._classname
       })}
       onClick={() => {
-        if (!isSelected && onChange) onChange();
+        if (!isSelected && !disabled && onChange) onChange();
       }}
     >
       {startIcon && startIcon}
       {label && label}
-    </div>
+      {tooltip &&
+        <Tooltip content={tooltip} placement='right'>
+          <CircleInformationIcon className="t-icon t-icon-muted cursor-pointer" />
+        </Tooltip>}
+    </div >
   );
 };
